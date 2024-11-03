8a27daec2c1736a6ab319028109c16170efeaeeb
Daroowala <153667031+Daroowala@users.noreply.github.com>
2024-11-04 02:08:46 +0500

    LINK OK

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Menu } from 'antd';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
     <Menu className='bg-neutral-300 font-bold ' mode="horizontal">
       <Menu.Item key="mail" icon={<MailOutlined />} style={{ color: 'blue', fontSize: '18px'}}>
-        Home
+       <Link to="/">Home</Link> 
       </Menu.Item>
       <Menu.Item key="app" icon={<AppstoreOutlined />} style={{ color: 'blue', fontSize: '18px' }}>
-        About
+      <Link to="/about">About</Link>
       </Menu.Item>
       <Menu.Item key="setting" icon={<SettingOutlined />}style={{ color: 'blue', fontSize: '18px' }}>
-        Contact
+      <Link to="/contact">Contact</Link>
       </Menu.Item>
     </Menu>
   );
